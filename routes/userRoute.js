392b911d9021df66ccce73dec6a82efe41bb3f3b
Aishwarya-Ramesh-Shetty <shettyaishu050@gmail.com>
2026-04-19 22:05:14 +0530

    Hashed the Password using Bcryptjs

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,21 +1,9 @@
 const express = require('express')
-const router = express.Router()
-const User = require('../models/User')
+const router = express.Router();
+const {registerUser} = require('../controllers/userController')
 
 
-router.post('/register',async(req,res)=>{
-    const {name,email,password} = req.body;
-    try{
-        const user = await User.create({
-            name,
-            email,
-            password
-        })
-        res.status(201).json(user);
-    }
-    catch(error){
-        res.status(400).json({message:error.message});
-    }
-})
+
+router.post('/register',registerUser);
 
 module.exports = router;
