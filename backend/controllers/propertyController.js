201cbc1f9e25c74983b6221c50d188f756a4e817
Aishwarya-Ramesh-Shetty <shettyaishu050@gmail.com>
2026-04-23 15:16:33 +0530

    Created Delete Property Api with Authorization

--- backend/controllers/propertyController.js
+++ backend/controllers/propertyController.js
@@ -15,4 +15,23 @@ const createProperty = async(req,res)=>{
     return res.status(201).json(property);
 };
 
-module.exports = {createProperty};
+const deleteProperty = async(req,res)=>{
+    const property = await Property.findById(req.params.id);
+
+    if(!property){
+        return res.status(400).json({messgae:"Property not found"})
+    }
+
+    if(property.owner.toString() != req.user._id){
+        return res.status(401).json({
+            message: "Not authorized to delete this property"
+        });
+    }
+
+    await property.deleteOne();
+    res.json({
+        message: "Property deleted successfully"
+    });
+}
+
+module.exports = {createProperty,deleteProperty};
--- backend/routes/propertyRoute.js
+++ backend/routes/propertyRoute.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router();
 const {protect} = require('../middleware/authMiddleware')
-const {createProperty} = require('../controllers/propertyController')
+const {createProperty,deleteProperty} = require('../controllers/propertyController')
 
 
 router.post('/',protect,createProperty);
+router.delete('/:id',protect,deleteProperty);
 
 module.exports = router;
