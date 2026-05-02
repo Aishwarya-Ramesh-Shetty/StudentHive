cd51673c1b7be53c0f44c90008ecfd95e38e2126
Aishwarya-Ramesh-Shetty <shettyaishu050@gmail.com>
2026-05-02 15:58:40 +0530

    Created Booking Aprrove and Reject Api

--- backend/controllers/bookingController.js
+++ backend/controllers/bookingController.js
@@ -23,4 +23,53 @@ const createBooking = async(req,res)=>{
     res.status(201).json(booking);
 }
 
-module.exports = {createBooking};
+
+const approveBooking = async(req,res)=>{
+    const booking = Booking.findById(req.params.id);
+
+    if(!booking){
+        return res.status(404).json({message:"Booking not found"})
+    }
+
+    if(booking.owner.toString()!==req.user._id.toString()){
+        return res.status(401).json({
+            message: "Not authorized to approve this booking"
+        });
+    }
+
+    booking.status = "approved";
+    await booking.save();
+
+    res.json({
+        messgae:"Booking aprroved",
+        booking
+    })
+}
+
+
+const rejectBooking = async(req,res)=>{
+    const booking = Booking.findById(req.params.id);
+
+    if(!booking){
+        return res.status(404).json({message:"Booking not found"})
+    }
+
+
+    if(booking.owner.toString()!==req.user._id.toString()){
+        return res.status(401).json({
+            message: "Not authorized to approve this booking"
+        });
+    }
+
+    booking.status = "rejected";
+    await booking.save();
+
+    res.json({
+        message: "Booking rejected",
+        booking
+    });
+
+
+}
+
+module.exports = {createBooking,approveBooking,rejectBooking};
--- backend/routes/bookingRoute.js
+++ backend/routes/bookingRoute.js
@@ -2,8 +2,10 @@ const express = require('express')
 const router = express.Router();
 const {protect} = require('../middleware/authMiddleware')
 
-const {createBooking} = require('../controllers/bookingController')
+const {createBooking,approveBooking,rejectBooking} = require('../controllers/bookingController')
 
 router.post('/bookings',protect,createBooking);
+router.put('/:id/aprrove',protect,approveBooking);
+router.put('/:id/reject',protect,rejectBooking);
 
 module.exports = router;
